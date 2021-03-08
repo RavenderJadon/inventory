10f878da8d70afa2b9eced5495738c2bcbe8b509
Ravender Jadon <ravender@jnexsoft.com>
2021-03-08 13:50:46 +0530

    five

---
+++ src/listitem/Listitem.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Image from "../images/product.png";
+
+const Listitem = () => {
+  return (
+    <div className="listitemdiv">
+      <p className="listitem">product name</p>
+      <p>avilable</p>
+    </div>
+  );
+};
+
+export default Listitem;
