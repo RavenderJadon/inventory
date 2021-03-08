174c08c7b09cb75afd8b1036d71a9a75a0c27ff3
Ravender Jadon <ravender@jnexsoft.com>
2021-03-08 13:50:22 +0530

    two

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Input from "./input/Input";
+import Listitem from "./listitem/Listitem";
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>ShopStock</h1>
+      <Input />
+      <Listitem />
     </div>
   );
 }
